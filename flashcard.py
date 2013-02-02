2c0337dae08829f462759378074ee0266d0cf7ac
Benjamin Osment <benjamin.j.osment@gmail.com>
2013-02-01 19:00:32 -0500

    add batch option

--- flashcard.py
+++ flashcard.py
@@ -38,7 +38,7 @@ def add_card():
     # TODO: need something like...initial setup? something that creates the tables
     
     print "Add cards, one at a time"
-    print "Ctrl-C to quit"
+    print "Type 'quit' or Ctrl-C to quit"
 
     try:
         db = connect_db()
@@ -50,6 +50,8 @@ def add_card():
         while True:
             
             front = raw_input("Front: ")
+            if front == 'quit':
+                break
             back = raw_input("Back: ")
             # issue SQL
             db.execute('insert into flashcard (front, back) values (?, ?)', 
@@ -67,10 +69,48 @@ def add_card():
         if db:
             db.close()
 
+
+def batch_add(filename):
+    """
+    Batch add a card. Read a colon deliminated file, adding card one at time
+    """
+    num_cards = 0
+    # open connection to DB
+    try:
+        db = connect_db()
+        cursor = db.cursor()
+    
+        # open file
+        # for each deliminated line, add a card
+        for line in open(filename):
+            line = line.strip()
+            front, back = line.split(':')
+            # issue SQL
+            db.execute('insert into flashcard (front, back) values (?, ?)', 
+                       (front, back))
+            num_cards += 1
+
+        # commit DB
+        db.commit()
+        # print statistics (num cards added)            
+        print "added %d cards" % num_cards
+            
+    except sqlite3.Error, e:
+        print "Error %s:" % e.args[0]
+        sys.exit(1)
+
+    # close connection
+    finally:
+        if db:
+            db.close()
+
+
 if __name__ == '__main__':
     # build the command line parser
+    # TODO: have a REVERSE option, where back is presented first
     parser = argparse.ArgumentParser(description='Quiz yourself on flashcards or add new cards')
     parser.add_argument('-a', '--add', help='add a new flashcard', action='store_true')
+    parser.add_argument('-b', '--batch', help='batch add a file')
     parser.add_argument('--verbose', action='store_true')    
 
     args = parser.parse_args(sys.argv[1:])
@@ -78,6 +118,11 @@ if __name__ == '__main__':
     #global verbose
     #verbose = args.verbose
 
+    if args.batch:
+        batch_add(args.batch)
+        sys.exit(0)
+        
     if args.add:
         add_card()
+        sys.exit(0)
 
